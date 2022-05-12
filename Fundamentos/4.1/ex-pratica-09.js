e9b3ecabad5bf8b932001b6b7f62b18b871b4bcf
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-12 17:50:43 -0300

    Realiza o exercício 9 do Bloco 4.1 em Exercícios > Agora na prática

---
+++ Fundamentos/4.1/ex-pratica-09.js
@@ -0,0 +1,10 @@
+const n1 = 11;
+const n2 = 9;
+const n3 = 12;
+
+(n1 % 2 != 0 || n2 % 2 != 0 || n3 % 2 != 0) ? console.log(true) : console.log(false);
+
+// com apenas um if 
+
+if ( n1 % 2 != 0 || n2 % 2 != 0 || n3 % 2 != 0 ) console.log(true);
+else console.log(false);
