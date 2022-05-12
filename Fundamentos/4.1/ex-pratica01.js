4e38e1d84eb9b8ccd421ee27c6e526780e9ad06c
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-12 15:48:51 -0300

    Realiza o primeiro exercício do Bloco 4.1 em Exercícios > Agora a Prática

---
+++ Fundamentos/4.1/ex-pratica01.js
@@ -0,0 +1,16 @@
+const a = 7;
+const b = 8;
+
+let soma = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+console.log(`A soma é ${soma}`);
+console.log(`A subtracao é ${subtracao}`);
+console.log(`A multiplicação é ${multiplicacao}`);
+console.log(`A divisão é ${divisao}`);
+console.log(`O resto da divisão de ${a} e ${b} é ${modulo}`);
+
+
