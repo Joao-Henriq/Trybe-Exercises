b0afb1a199e6ae2e864966a1b5de1b578dece3bd
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-12 16:12:02 -0300

    Realiza o exercício 5 do Bloco 4.1 em Exercícios > Agora a Prática

---
+++ Fundamentos/4.1/ex-pratica-05.js
@@ -0,0 +1,6 @@
+const angulo1 = 30;
+const angulo2 = 40;
+const angulo3 = 90;
+
+if ( angulo1 + angulo2 + angulo3 === 180 && angulo1 < 0 && angulo2 < 0 && angulo3 < 0) console.log(true)
+else console.log(false);
