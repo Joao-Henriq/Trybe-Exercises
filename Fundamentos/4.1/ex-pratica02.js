9b41873f4a5066947f86a744b3cb59fd69b2e2a7
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-12 17:31:36 -0300

    Realiza o exercício 7 do Bloco 4.1 em Exercícios > Agora a prática

--- Fundamentos/4.1/ex-pratica02.js
+++
@@ -1,5 +0,0 @@
-const n1 = 5;
-const n2 = 10;
-
-if ( n1 > n2 ) console.log('O valor maior é:',n1);
-else console.log('O valor maior é:', n2);
--- Fundamentos/4.1/ex-pratica03.js
+++
@@ -1,7 +0,0 @@
-const n1 = 4;
-const n2 = 5;
-const n3 = 6;
-
-if ( n1 > (n2 && n3)) console.log('O maior número é:', n1);
-else if ( n2 > (n1 && n3) ) console.log('O maior número é:', n2);
-else console.log('O maior número é:', n3);
