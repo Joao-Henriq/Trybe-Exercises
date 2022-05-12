2dd17952e6a90c8f0c631cc47e9e2bf92e26bcf2
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-12 17:43:56 -0300

    Realiza o exercício 8 do Bloco 4.1 em Exercícios > Agora a prática

--- Fundamentos/4.1/ex-pratica-08.js
+++ Fundamentos/4.1/ex-pratica-08.js
@@ -1,6 +1,10 @@
 const n1 = 1;
 const n2 = 5;
-const n3 = 8;
+const n3 = 9;
 
 if ( n1 % 2 == 0 || n2 % 2 == 0 || n3 % 2 == 0) console.log(true);
 else console.log(false);
+
+// com operador ternário 
+
+(n1 % 2 == 0 || n2 % 2 == 0 || n3 % 2 == 0) ? console.log(true) : console.log(false);
