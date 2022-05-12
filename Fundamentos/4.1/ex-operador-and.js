62ae879f0c141ed7b39d27b6bc60c784ad8c4999
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-12 15:28:02 -0300

    Realiza o exercício do operador AND

---
+++ Fundamentos/4.1/ex-operador-and.js
@@ -0,0 +1,10 @@
+const currentHour = 22; // algum número entre 4 e 24.
+let message = '';       // string vazia
+
+if ( currentHour >= 22 ) console.log('Não devemos comer nada, é hora de dormir');
+else if (currentHour >= 18 && currentHour < 22) message += 'Rango da noite, vamos jantar :D';
+else if (currentHour >= 14 && currentHour < 18) message += 'Vamos fazer um bolo pro café da tarde?';
+else if (currentHour > 11 && currentHour < 14) message += 'Hora do almoço';
+else if (currentHour > 4 && currentHour < 11) message += 'Hmmm, cheiro de café recém passado';
+
+console.log(message);
