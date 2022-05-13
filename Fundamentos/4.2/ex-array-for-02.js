964cadc064c50e9ede40e65e178d7274ff875873
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-13 19:16:47 -0300

    Realiza o exercício 2 do Bloco 4.2 em Exercícios > Agora a Prática

---
+++ Fundamentos/4.2/ex-array-for-02.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let number of numbers){
+    soma += number
+}
+console.log(soma);
+//git commit -m "Realiza o exercício 1 do Bloco 4.2 em Exercícios > Agora a Prática"
