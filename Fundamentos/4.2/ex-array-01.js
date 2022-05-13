2ed02ab55f9d9b63641e203764ffc5c6681a3d91
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-13 17:24:33 -0300

    Realiza o exercício 1 do Bloco 4.2 em Conteudos > Arrays

---
+++ Fundamentos/4.2/ex-array-01.js
@@ -0,0 +1,12 @@
+let menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
+let menuServices = menu[3];
+console.log(menuServices)
+
+menuServices = menu[2]
+console.log(menuServices);
+
+menuServices = menu[1];
+console.log(menuServices);
+
+menuServices = menu[0];
+console.log(menuServices);
