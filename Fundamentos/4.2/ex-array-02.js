c0f8f71e64362ef0b5d3d4ae7af8a4422ce89cf6
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-13 17:31:48 -0300

    Realiza o exercício 2 do Bloco 4.2 em Conteúdos > Arrays

---
+++ Fundamentos/4.2/ex-array-02.js
@@ -0,0 +1,13 @@
+let menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
+let indexOfPortifolio =  menu[0]
+console.log(indexOfPortifolio);
+
+indexOfPortifolio =  menu[1];
+console.log(indexOfPortifolio);
+
+
+console.log(indexOfPortifolio);
+indexOfPortifolio =  menu[2]
+
+indexOfPortifolio =  menu[3];
+console.log(indexOfPortifolio);
