a70f021124667209f7d66269ca00ad4797996fba
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-14 01:11:53 -0300

    Realiza o exercício 4 do Bloco 4.2 em Exercícios > Agora a Prática

---
+++ Fundamentos/4.2/ex-array-for-04.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+for (let number of numbers){
+    soma += number;
+}
+let media = soma / numbers.length
+console.log(`A média é ${media}`);
+if ( media > 20) console.log('Valor maior que 20')
+else console.log('Valor menor que 20');
+
