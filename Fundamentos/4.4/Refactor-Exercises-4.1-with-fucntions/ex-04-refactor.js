bb44446baaa3f523c2bb10fac8a84678c63d18ae
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-21 17:48:57 -0300

    Realiza o exercício 4 do Bloco 4.1 utilizando funções

---
+++ Fundamentos/4.4/Refactor-Exercises-4.1-with-fucntions/ex-04-refactor.js
@@ -0,0 +1,6 @@
+function isPositiveNegativeOrZero(a){
+    if (a > 0) console.log('positivo');
+    else if (a < 0) console.log('negativo');
+    else console.log('zero');
+}
+isPositiveNegativeOrZero(-3);
