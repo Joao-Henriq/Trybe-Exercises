cf67a8f79066477c716397dad79db3e9c16202b8
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-21 18:35:43 -0300

    Realiza o exercício 01 do Bloco 4.4 em Exercícios > Forin For of Objetos

---
+++ Fundamentos/4.4/Exercises-ForIn-ForOf/exercises-01.js
@@ -0,0 +1,9 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+for(i in info){
+    console.log('Bem vindo(a)', info[i]);
+}
