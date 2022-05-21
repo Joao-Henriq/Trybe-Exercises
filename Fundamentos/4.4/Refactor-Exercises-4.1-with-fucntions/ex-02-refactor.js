5c6b6df6311f355cfb37874369eb14598ffa1ca7
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-21 17:38:34 -0300

    Realiza o exercício 2 do Bloco 4.1 usando funções

---
+++ Fundamentos/4.4/Refactor-Exercises-4.1-with-fucntions/ex-02-refactor.js
@@ -0,0 +1,7 @@
+function MaiorDosDoisNumeros(a,b){
+    if(a > b) console.log(`${a} é maior que ${b}`);
+    else if (a < b) console.log(`${b} é maior que ${a}`);
+    else console.log(`${a} e ${b} são iguais`);
+    return;
+}
+MaiorDosDoisNumeros(10,10);
