e335b60aa586d2f84a9994476b3aacac1c93cb22
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-21 17:32:42 -0300

    Realiza o primeiro exercício do Bloco 4.1 com funções

---
+++ Fundamentos/4.4/Refactor-Exercises-4.1-with-fucntions/ex-01-refator.js
@@ -0,0 +1,29 @@
+function soma(a,b){
+    let soma = a+b;
+    return soma;
+}
+
+function subtracao(a,b){
+    let subtracao = a-b;
+    return subtracao;
+}
+
+function multiplicacao(a,b){
+    let multiplicacao = a * b;
+    return multiplicacao;
+}
+
+function divisao(a,b){
+    let divisao = a/b;
+    return divisao;
+}
+
+function modulo(a,b){
+    let modulo = a % b;
+    return modulo;    
+}
+console.log('A soma é:',soma(4,6));
+console.log('A subtração é',subtracao(4,6));
+console.log('A multiplicacão é',multiplicacao(4,6));
+console.log('A divisão é: ',divisao(4,6));
+console.log('O módulo é: ',modulo(4,6));
