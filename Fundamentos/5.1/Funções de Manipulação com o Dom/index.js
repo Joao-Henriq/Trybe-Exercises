1375a84fec94fe7b3d3492993397e6eba1ddd895
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-24 10:52:35 -0300

    Realiza o requisito 2 do exercício

--- Fundamentos/5.1/Funções de Manipulação com o Dom/index.js
+++ Fundamentos/5.1/Funções de Manipulação com o Dom/index.js
@@ -1,3 +1,7 @@
 // requisito 1
 let myParagraphsOfSection = document.getElementsByTagName('p');
 myParagraphsOfSection[1].innerText = 'Eu me vejo daqui a 2 anos como um desenvolvedor full stack atuando no mercado de trabalho';
+
+// requisito 2
+let myBackgroundOfMainContent = document.getElementsByClassName('main-content');
+myBackgroundOfMainContent[0].style.backgroundColor = 'rgb(76,164,109)';
