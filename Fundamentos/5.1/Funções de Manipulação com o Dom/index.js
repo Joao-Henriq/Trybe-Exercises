79bd5b973c10edfe62c6f450dd2978498345bb41
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-24 10:48:46 -0300

    Realiza o requisito 1 do exercício

---
+++ Fundamentos/5.1/Funções de Manipulação com o Dom/index.js
@@ -0,0 +1,3 @@
+// requisito 1
+let myParagraphsOfSection = document.getElementsByTagName('p');
+myParagraphsOfSection[1].innerText = 'Eu me vejo daqui a 2 anos como um desenvolvedor full stack atuando no mercado de trabalho';
