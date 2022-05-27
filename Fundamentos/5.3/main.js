88a67cbe56b33989ba56c9efe99c39a7129f777a
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-26 23:37:36 -0300

    Realiza todos os requisitos

--- Fundamentos/5.3/main.js
+++ Fundamentos/5.3/main.js
@@ -48,10 +48,17 @@ function changeText(event) {
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+myWebpage.addEventListener('dblclick', function(event){
+  window.location.href = ("https://joao-henriq.github.io/")
+  console.log(event.target)
+});
+
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
-
+myWebpage.addEventListener('mouseover', function(event){
+  myWebpage.style.background = 'green'
+})
 // Segue abaixo um exemplo do uso de event.target:
 
 function resetText(event) {
