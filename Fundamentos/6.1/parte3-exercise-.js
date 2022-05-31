af951b11faf984c7bae7d0a0beb01c8ec5a50075
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-31 16:43:32 -0300

    Realiza o exercício da Parte 3 do Bloco 6.1

---
+++ Fundamentos/6.1/parte3-exercise-.js
@@ -0,0 +1,12 @@
+// SELETORES
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+HREF_LINK.addEventListener('click', (_event) =>{
+    _event.preventDefault();
+});
+
+INPUT_CHECKBOX.addEventListener('click', (_event) => {
+    _event.preventDefault();
+});
