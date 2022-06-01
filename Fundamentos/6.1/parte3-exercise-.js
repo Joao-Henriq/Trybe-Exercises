32f67243878ed44b8ab108b9ffd55467247d3dbf
João Henrique <joaohenriqueblocked@gmail.com>
2022-05-31 21:09:02 -0300

    Realiza o exerício da parte 4 do Bloco 6.1

--- Fundamentos/6.1/parte3-exercise-.js
+++ Fundamentos/6.1/parte3-exercise-.js
@@ -10,3 +10,8 @@ HREF_LINK.addEventListener('click', (_event) =>{
 INPUT_CHECKBOX.addEventListener('click', (_event) => {
     _event.preventDefault();
 });
+
+INPUT_TEXT.addEventListener('keypress', (_event) => {
+    if(_event.key !== 'a' ) _event.preventDefault();
+
+});
