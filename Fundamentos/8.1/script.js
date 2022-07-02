06861dfcb4c26cb04db9d895513276c6a840677c
João Henrique <joaohenriqueblocked@gmail.com>
2022-07-02 17:47:06 -0300

    Realiza o exercício doingThings

---
+++ Fundamentos/8.1/script.js
@@ -0,0 +1,9 @@
+const wakeUp = () => console.log('Acordando!!');
+
+const drinkCoffee = () => console.log('Bora tomar café !!');
+
+const letSleep = () => console.log('Partiu dormir!!');
+
+const doingThings = (param) => param;
+
+doingThings(wakeUp());
