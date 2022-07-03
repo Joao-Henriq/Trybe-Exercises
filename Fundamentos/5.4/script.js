94f8c570dfca7de04e0f91f8cdee34cf124aba27
João Henrique <joaohenriqueblocked@gmail.com>
2022-07-03 05:10:20 -0300

    .

--- Fundamentos/5.4/script.js
+++ Fundamentos/5.4/script.js
@@ -6,30 +6,43 @@ const fontStyle = document.querySelector('.inputFontStyle');
 const button = document.querySelector('.button')
 const body = document.querySelector('body');
 
-function changeBackground() {
-  body.style.background = background.value;
-}
+const settings = {
+  background: body.style.background = background.value,
+  color: body.style.color = colorText.value,
+  fontSize: body.style.fontSize = `${fontSize.value}px`,
+  lineheight: body.style.lineHeight = `${lineHeight.value}px`,
+  fontStyle: body.style.fontStyle = fontStyle.value
+};
+// function changeBackground() {
+//   body.style.background = background.value;
+// }
 
-function changeColorText() {
-  body.style.color = colorText.value;
-}
+// function changeColorText() {
+//   body.style.color = colorText.value;
+// }
 
-function changeFontSize() {
-  body.style.fontSize = `${fontSize.value}px`;
-}
+// function changeFontSize() {
+//   body.style.fontSize = `${fontSize.value}px`;
+// }
 
-function changeLineHeight() {
-  body.style.lineHeight = `${lineHeight.value}px`
-}
+// function changeLineHeight() {
+//   body.style.lineHeight = `${lineHeight.value}px`
+// }
 
-function changefontStyle() {
-  body.style.fontStyle = fontStyle.value;
-}
+// function changefontStyle() {
+//   body.style.fontStyle = fontStyle.value;
+// }
 
 button.addEventListener('click', () => {
-  changeBackground();
-  changeColorText();
-  changeFontSize();
-  changeLineHeight(); 
-  changefontStyle();
+  for (let key in settings) {
+    console.log(settings[key]);
+  }
 });
+
+localStorage.setItem('background', JSON.stringify(`${background.value}`));
+localStorage.setItem('color', JSON.stringify(`${colorText.value}`));
+localStorage.setItem('fontsize', JSON.stringify(`${fontSize.value}`));
+localStorage.setItem('lineheight', JSON.stringify(`${lineHeight.value}`));
+localStorage.setItem('fontstyle', JSON.stringify(`${fontStyle.value}`));
+
+localStorage.getItem('color');
