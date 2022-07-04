ba7ec81cf24f0ca5dc33e4fbc41496f51bb1b44c
João Henrique <joaohenriqueblocked@gmail.com>
2022-07-04 00:44:17 -0300

    Finaliza o dia 8.2

--- Fundamentos/8.2/find.js
+++ Fundamentos/8.2/find.js
@@ -1,4 +1,4 @@
-const numbers = [19, 21, 30, 3, 45, 22, 15];
+  const numbers = [19, 21, 30, 3, 45, 22, 15];
 
 const findDivisibleBy3And5 = () => numbers.find((number) => number % 3 === 0 && number % 5 === 0); 
 
